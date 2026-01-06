1a7b069f4fb929b1454567bf95b2bf19c1e8dfe6
Jnkgb <pjw0918jw@naver.com>
2026-01-06 10:28:32 +0900

    초기 설정

---
+++ src/main/java/org/example/gaekdambe/GaekdamBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.gaekdambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaekdamBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GaekdamBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/gaekdambe/GaekdamBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gaekdambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaekdamBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
