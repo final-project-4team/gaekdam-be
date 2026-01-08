d21fb65765586244a7dd6d2cb5e1e33e987c4475
Kim-sangjae <kimsangjae94114@gmail.com>
2026-01-08 20:39:45 +0900

    feat: 패키지 , 패키지부대시설 엔티티 생성 , 패키지 더미데이터 50 생성

--- src/main/java/com/gaekdam/gaekdambe/reservation_service/reservation/command/domain/entity/ReservationPackage.java
+++ src/main/java/com/gaekdam/gaekdambe/reservation_service/reservation/command/domain/entity/ReservationPackage.java
@@ -6,30 +6,54 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "reservation_pakage")
+@Table(name = "reservation_package")
 public class ReservationPackage {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "package_code" , nullable = false)
+    @Column(name = "package_code")
     private Long packageCode;
 
-    @Column(name = "package_name" , nullable = false , length = 50)
+    @Column(name = "package_name", nullable = false, length = 50)
     private String packageName;
 
-    @Column(name = "package_content" , nullable = false)
+    @Column(name = "package_content", nullable = false, length = 255)
     private String packageContent;
 
-    @Column(name = "facility_code")
-    private Long facilityCode;
+    @Column(name = "package_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal packagePrice;
 
-    // 추후 엔티티 생성 다 되면 fk 컬럼으로 변경
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "facility_code", nullable = false)
-//    private Facility facility;
+    @Column(name = "hotel_group_code", nullable = false)
+    private Long hotelGroupCode;
 
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public static ReservationPackage createReservationPackage(
+            String name,
+            String content,
+            BigDecimal price,
+            Long hotelGroupCode
+    ) {
+        LocalDateTime now = LocalDateTime.now();
+        return ReservationPackage.builder()
+                .packageName(name)
+                .packageContent(content)
+                .packagePrice(price)
+                .hotelGroupCode(hotelGroupCode)
+                .createdAt(now)
+                .updatedAt(now)
+                .build();
+    }
 }
