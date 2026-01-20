f8dd996885377381d28a0ba4192f8d644aa89966
silentanderson <anderson91@naver.com>
2026-01-20 10:46:05 +0900

    feat: ReportKPITargetController 업데이트

--- src/main/java/com/gaekdam/gaekdambe/analytics_service/report/dataset/api/ReportKPITargetController.java
+++ src/main/java/com/gaekdam/gaekdambe/analytics_service/report/dataset/api/ReportKPITargetController.java
@@ -2,10 +2,21 @@ package com.gaekdam.gaekdambe.analytics_service.report.dataset.api;
 
 import java.util.List;
 
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.gaekdam.gaekdambe.analytics_service.report.dataset.command.application.dto.*;
+import com.gaekdam.gaekdambe.analytics_service.report.dataset.command.application.dto.ReportKPITargetCreateDto;
+import com.gaekdam.gaekdambe.analytics_service.report.dataset.command.application.dto.ReportKPITargetResponseDto;
+import com.gaekdam.gaekdambe.analytics_service.report.dataset.command.application.dto.ReportKPITargetUpdateDto;
 import com.gaekdam.gaekdambe.analytics_service.report.dataset.command.application.service.ReportKPITargetService;
 import com.gaekdam.gaekdambe.analytics_service.report.dataset.command.domain.entity.ReportKPITargetId;
 import com.gaekdam.gaekdambe.global.config.model.ApiResponse;
