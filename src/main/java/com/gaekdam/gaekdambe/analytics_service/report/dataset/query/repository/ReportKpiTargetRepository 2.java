a90907bd1821e5ad0366b2125c503d3c10285545
silentanderson <anderson91@naver.com>
2026-01-21 17:24:09 +0900

    fix: 전체요약 계산 오류 로직 해결

--- src/main/java/com/gaekdam/gaekdambe/analytics_service/report/dataset/query/repository/ReportKpiTargetRepository 2.java
+++
@@ -1,6 +0,0 @@
-package com.gaekdam.gaekdambe.analytics_service.report.dataset.query.repository;
-
-public interface ReportKpiTargetRepository extends JpaRepository<ReportKpiTarget, Long> {
-    Optional<ReportKpiTarget> findFirstByMetricKeyAndPeriod(String metricKey, String period);
-    Optional<ReportKpiTarget> findFirstByMetricKeyOrderByCreatedAtDesc(String metricKey);
-}
