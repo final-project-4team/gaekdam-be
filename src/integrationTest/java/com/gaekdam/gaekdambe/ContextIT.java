58a736e31e466f40307ad5ce3922f77d2ced806b
kim100000000 <kst980510@gmail.com>
2026-01-30 20:15:39 +0900

    feat : testContainer 설정 추가

--- src/integrationTest/java/com/gaekdam/gaekdambe/ContextIT.java
+++ src/integrationTest/java/com/gaekdam/gaekdambe/ContextIT.java
@@ -1,4 +1,13 @@
 package com.gaekdam.gaekdambe;
 
-public class ContextIT {
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextIT extends IntegrationTestBase {
+
+    @Test
+    void contextLoads() {
+        // 스프링 + DB 컨테이너 뜨면 성공
+    }
 }
