131a30f02f6d1e4de890da7200b43899cd41659a
Jnkgb <pjw0918jw@naver.com>
2026-02-03 14:26:20 +0900

    feat : swagger 추가

--- src/main/java/com/gaekdam/gaekdambe/global/HealthCheckController.java
+++ src/main/java/com/gaekdam/gaekdambe/global/HealthCheckController.java
@@ -1,15 +1,14 @@
 package com.gaekdam.gaekdambe.global;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Tag(name="헬스 체크")
 @RestController
